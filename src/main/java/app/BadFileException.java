fe710a68972d3408686a24ed40bb6237814cf273
CalD2 <95912602+CalD2@users.noreply.github.com>
2022-04-19 12:02:58 +0200

    reset git

---
+++ src/main/java/app/BadFileException.java
@@ -0,0 +1,15 @@
+package app;
+
+/**
+ * Fichier invalide
+ *
+ */
+public class BadFileException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public BadFileException()
+	{
+		new Exception("fichier invalide");
+	}
+}
